84592b57651f3ddd67e23129d60f88678060cf8e
maneeshtiwari007 <maneeshtiwari007@gmail.com>
2024-01-31 18:21:56 +0530

    commit changes
    commit changes

--- src/Components/ProfServicesComponent.tsx
+++ src/Components/ProfServicesComponent.tsx
@@ -1,7 +1,7 @@
 import { Component, ReactNode } from "react";
 import ScreenInterfcae from "../Interfaces/Common/ScreensInterface";
 import CommonScreenStateInterface from "../Interfaces/States/CommonScreenStateInterface";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, Pressable, ScrollView, Text, View } from "react-native";
 import { ThemeStyling } from "../utilty/styling/Styles";
 import Colors from "../utilty/Colors";
 import CheckBox from 'expo-checkbox';
@@ -18,40 +18,46 @@ export default class ProfServicesComponent extends Component<ScreenInterfcae, Co
         //console.log(this.props?.data?.categories)
     }
     addServiceToCart(data) {
-        console.log(data)
-        const dataObj = (this.state.dataObj) ? this.state.dataObj : [];
-        dataObj.push(data);
-        console.log(dataObj)
+        let dataObj = (this.state.dataObj) ? this.state.dataObj : [];
+        const indexObj = dataObj?.indexOf(data);
+        if (dataObj?.indexOf(data) >= 0) {
+            dataObj?.splice(indexObj, 1)
+        } else {
+            dataObj.push(data);
+        }
         this.setState({ dataObj: dataObj })
     }
     render() {
         return <ScrollView style={{ paddingTop: 10 }}>
-            <View style={{backgroundColor:Colors.primary_light_color, padding:3, paddingTop:5,marginBottom:5 , justifyContent:"center", alignItems:"center"}}>
-                <Text style={[ThemeStyling.heading5, {margin:0, color:Colors.primary_color}]}>Hair Cut</Text>
-            </View>
             {this.props?.data?.categories?.length > 0 && this.props?.data?.categories?.map((item: any, index: number) => {
                 if (item?.services) {
-                    return item?.services?.map((itemObj: any, indexService: number) => {
-                        return <View style={{ padding: 15, paddingVertical: 5 }} key={index + indexService}>
-                            <View style={[ThemeStyling.twoColumnLayout]}>
-                                <View style={{ marginRight: 10 }}>
-                                    <View style={{ padding: 8, borderRadius: 100, shadowColor: '#000', shadowOffset: { width: -2, height: 4 }, shadowOpacity: 0.4, shadowRadius: 10, backgroundColor: Colors.white }}>
-                                        <Image style={[ThemeStyling.cardImage, { width: 40, height: 40, borderRadius: 10 }]} source={{ uri: itemObj?.icon }} />
+                    return <>
+                        <View key={index} style={{ backgroundColor: Colors.primary_light_color, padding: 3, paddingTop: 5, marginBottom: 5, justifyContent: "center", alignItems: "center" }}>
+                            <Text style={[ThemeStyling.heading5, { margin: 0, color: Colors.primary_color }]}>{item?.name}</Text>
+                        </View>
+                        {item?.services?.map((itemObj: any, indexService: number) => {
+                            return <View style={{ padding: 15, paddingVertical: 5 }} key={index + indexService}>
+                                <View style={[ThemeStyling.twoColumnLayout]}>
+                                    <View style={{ marginRight: 10 }}>
+                                        <View style={{ padding: 8, borderRadius: 100, shadowColor: '#000', shadowOffset: { width: -2, height: 4 }, shadowOpacity: 0.4, shadowRadius: 10, backgroundColor: Colors.white }}>
+                                            <Image style={[ThemeStyling.cardImage, { width: 40, height: 40, borderRadius: 10 }]} source={{ uri: itemObj?.icon }} />
+                                        </View>
                                     </View>
-                                </View>
-                                <View style={[ThemeStyling.col6, { marginRight: 10 }]}>
-                                    <Text style={[ThemeStyling.heading5, { fontSize: Colors.FontSize.h6, fontWeight: '600', color: Colors.dark_color, marginBottom: 0 }]}>{itemObj?.service_name}</Text>
-                                    <Text style={[ThemeStyling.text2, { fontSize: Colors.FontSize.f12, color: Colors.secondry_color }]}>Cleaning up the necklines, touch up sidebuns</Text>
-                                </View>
-                                <View style={[ThemeStyling.col2, { flex: 1, alignItems: "flex-end" }]}>
-                                    <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-                                        <Text style={[ThemeStyling.heading5, { fontSize: Colors.FontSize.h6, fontWeight: '600', color: Colors.dark_color, marginBottom: 0, marginRight: 5 }]}>{CommonHelper.returnPriceWithCurrency(itemObj?.price)}</Text>
-                                        <CheckBox color={Colors.primary_color} style={[{ width: 15, height: 15, borderColor: Colors.secondry_color, borderWidth: 1, backgroundColor: 'green' }]} onValueChange={() => { this.addServiceToCart(itemObj?.service_id) }} />
+                                    <View style={[ThemeStyling.col6, { marginRight: 10 }]}>
+                                        <Text style={[ThemeStyling.heading5, { fontSize: Colors.FontSize.h6, fontWeight: '600', color: Colors.dark_color, marginBottom: 0 }]}>{itemObj?.service_name}</Text>
+                                        <Text style={[ThemeStyling.text2, { fontSize: Colors.FontSize.f12, color: Colors.secondry_color }]}>Cleaning up the necklines, touch up sidebuns</Text>
                                     </View>
+                                    <Pressable style={[ThemeStyling.col2, { flex: 1, alignItems: "flex-end" }]} onPress={() => { this.addServiceToCart(itemObj?.service_id) }}>
+                                        <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+                                            <Text style={[ThemeStyling.heading5, { fontSize: Colors.FontSize.h6, fontWeight: '600', color: Colors.dark_color, marginBottom: 0, marginRight: 5 }]}>{CommonHelper.returnPriceWithCurrency(itemObj?.price)}</Text>
+                                            <CheckBox value={(this.state?.dataObj?.indexOf(itemObj?.service_id) >= 0) ? true : false} color={Colors.primary_color} style={[{ width: 15, height: 15, borderColor: Colors.secondry_color, borderWidth: 1, backgroundColor: 'green' }]} />
+                                        </View>
+                                    </Pressable>
                                 </View>
                             </View>
-                        </View>
-                    })
+                        })
+                        }
+                    </>
                 }
 
             })}
--- src/Screens/User/ProfDetail.tsx
+++ src/Screens/User/ProfDetail.tsx
@@ -27,87 +27,104 @@ export default class ProfDetail extends Component<ScreenInterfcae, CommonScreenS
                 { key: '1', title: 'Services' },
                 { key: '2', title: 'Information' },
                 { key: '3', title: 'Review' },
-              ],
+            ],
         }
     }
     async componentDidMount() {
         this.setState({ loader: true });
         await this.getApiData()
     }
-    async getApiData(){
+    async getApiData() {
         const id = this.props.route?.params?.data?.id;
-        CommonApiRequest.getProfDetails(id).then((response)=>{
+        CommonApiRequest.getProfDetails(id).then((response) => {
             this.setState({ loader: false });
-            console.log(response);
-            if(response?.status===200){
-                this.setState({dataObj:response?.results});
+            if (response?.status === 200) {
+                this.setState({ dataObj: response?.results });
             }
-        }).then((error)=>{
+        }).then((error) => {
             this.setState({ loader: false });
         })
     }
     _renderScene = ({ route }) => {
         switch (route.key) {
-        case '1':
-          return <ProfServicesComponent data={this.state?.dataObj}></ProfServicesComponent>;
-        case '2':
-          return <ProfInformationComponent></ProfInformationComponent>;
-        case '3':
-          return <ProfReviewComponent></ProfReviewComponent>;
-        default:
-          return null;
+            case '1':
+                return <ProfServicesComponent data={this.state?.dataObj}></ProfServicesComponent>;
+            case '2':
+                return <ProfInformationComponent></ProfInformationComponent>;
+            case '3':
+                return <ProfReviewComponent></ProfReviewComponent>;
+            default:
+                return null;
         }
-      };
-      _renderTabBar = (props) => (
+    };
+    _renderTabBar = (props) => (
         <TabBar
             {...props}
             activeColor={Colors.primary_color}
             inactiveColor={Colors.secondry_color}
-            style={{backgroundColor:'white',paddingTop:5,paddingBottom:5,borderColor:Colors.primary_color}}
+            style={{ backgroundColor: 'white', paddingTop: 5, paddingBottom: 5, borderColor: Colors.primary_color }}
         />
     );
-    
+
     render() {
         return (
-            <MainLayout onRefresh={() => { }} headerText="" loader={this.state?.loader} containerStyle={{ paddingTop: 10 }} navigation={this.props.navigation}  route={this.props.route}>
-                <View>
-                    <ImageBackground source={require('../../../assets/staticimages/thumbnail4.jpg')} resizeMode="cover" style={{ flex: 1, height: 150 }}></ImageBackground>
-                </View>
-                <View style={[ThemeStyling.card, { backgroundColor: Colors.gray200, borderRadius: 0, marginBottom: 0 }]}>
-                    <View style={[ThemeStyling.cardBody, { paddingBottom: 10 }]}>
-                        <View style={[ThemeStyling.twoColumnLayout,]}>
-                            <View style={[ThemeStyling.col8,{borderRightColor:Colors.gray400, borderStyle:"solid", borderRightWidth:1}]}>
-                                <Text style={[ThemeStyling.heading5, { fontWeight: '600', color: Colors.dark_color, marginBottom: 3 }]}>The Big Tease Salon</Text>
-                                <View style={[ThemeStyling.starRating, { marginBottom: 5 }]}>
-                                    <FontAwesome style={ThemeStyling.iconStar} name="star" color={Colors.primary_color} />
-                                    <FontAwesome style={ThemeStyling.iconStar} name="star" color={Colors.primary_color} />
-                                    <FontAwesome style={ThemeStyling.iconStar} name="star" color={Colors.primary_color} />
-                                    <FontAwesome style={ThemeStyling.iconStar} name="star" color={Colors.gray400} />
-                                    <FontAwesome style={ThemeStyling.iconStar} name="star" color={Colors.gray400} />
+            <MainLayout onRefresh={() => { }} headerText="" loader={this.state?.loader} containerStyle={{ paddingTop: 0.5 }} navigation={this.props.navigation} route={this.props.route}>
+                {this.state?.dataObj &&
+                    <>
+                        <View>
+                            <ImageBackground source={{ uri: this.state?.dataObj?.users?.photo }} resizeMode="cover" style={{ flex: 1, height: 150, width: '100%' }}></ImageBackground>
+                        </View>
+                        <View style={[ThemeStyling.card, { backgroundColor: Colors.gray200, borderRadius: 0, marginBottom: 0 }]}>
+                            <View style={[ThemeStyling.cardBody, { paddingBottom: 10 }]}>
+                                <View style={[ThemeStyling.twoColumnLayout,]}>
+                                    <View style={[ThemeStyling.col8, { borderRightColor: Colors.gray400, borderStyle: "solid", borderRightWidth: 1 }]}>
+                                        <Text style={[ThemeStyling.heading5, { fontWeight: '600', color: Colors.dark_color, marginBottom: 3 }]}>{this.state?.dataObj?.users?.name}</Text>
+                                        <View style={[ThemeStyling.starRating, { marginBottom: 5 }]}>
+                                            <FontAwesome style={ThemeStyling.iconStar} name="star" color={Colors.primary_color} />
+                                            <FontAwesome style={ThemeStyling.iconStar} name="star" color={Colors.primary_color} />
+                                            <FontAwesome style={ThemeStyling.iconStar} name="star" color={Colors.primary_color} />
+                                            <FontAwesome style={ThemeStyling.iconStar} name="star" color={Colors.gray400} />
+                                            <FontAwesome style={ThemeStyling.iconStar} name="star" color={Colors.gray400} />
+                                        </View>
+                                        <View style={{ flexDirection: "row" }}>
+                                            <View><Ionicons name="location-outline" size={15} style={{ color: Colors.secondry_color }} /></View>
+                                            <Text style={[ThemeStyling.text2, { color: Colors.secondry_color }]}>{this.state?.dataObj?.users?.user_professional_details?.location}</Text>
+                                        </View>
+                                    </View>
+                                    <View style={[ThemeStyling.col4, { alignItems: "flex-start", paddingLeft: 10 }]}>
+                                        <Text style={[ThemeStyling.text2, { fontSize: 11, color: Colors.secondry_color }]}>
+                                            {this.state?.dataObj && this.state?.dataObj?.users?.live &&
+                                                <AntDesign name="checkcircle" size={11} color={Colors.success_color} />
+                                            }
+                                            {this.state?.dataObj && !this.state?.dataObj?.users?.live &&
+                                                <FontAwesome5 name="times-circle" size={11} color={Colors.primary_color} />
+                                            }
+                                            &nbsp;Live Booking
+                                        </Text>
+                                        <Text style={[ThemeStyling.text2, { fontSize: 11, color: Colors.secondry_color }]}>
+                                            {this.state?.dataObj && this.state?.dataObj?.users?.schedule &&
+                                                <AntDesign name="checkcircle" size={11} color={Colors.success_color} />
+                                            }
+                                            {this.state?.dataObj && !this.state?.dataObj?.users?.schedule &&
+                                                <FontAwesome5 name="times-circle" size={11} color={Colors.primary_color} />
+                                            }
+                                            &nbsp;Schedule Booking
+                                        </Text>
+                                    </View>
                                 </View>
-                                <View style={{ flexDirection: "row" }}>
-                                    <View><Ionicons name="location-outline" size={15} style={{ color: Colors.secondry_color }} /></View>
-                                    <Text style={[ThemeStyling.text2, { color: Colors.secondry_color }]}>4140 Parker Rd. Allortown</Text>
-                                </View>
-                            </View>
-                            <View style={[ThemeStyling.col4, { alignItems: "flex-start", paddingLeft:10 }]}>
-                                <Text style={[ThemeStyling.text2, { fontSize: 11, color: Colors.secondry_color }]}>
-                                    <AntDesign name="checkcircle" size={11} color={Colors.success_color} />&nbsp;Live Booking</Text>
-                                <Text style={[ThemeStyling.text2, { fontSize: 11, color: Colors.secondry_color }]}>
-                                    <FontAwesome5 name="times-circle" size={11} color={Colors.primary_color} />&nbsp;Schedule Booking</Text>
                             </View>
                         </View>
-                    </View>
-                </View>
-                <TabView
-                    navigationState={{index:this.state.index,routes:this.state.routes}}
-                    renderScene={this._renderScene}
-                    onIndexChange={index => this.setState({ index })}
-                    initialLayout={{ width: Dimensions.get('window').width }}
-                    style={{ height:Dimensions.get('screen').height/2-90 }}
-                    renderTabBar={this._renderTabBar}
-                    
-                />
+                        <TabView
+                            navigationState={{ index: this.state.index, routes: this.state.routes }}
+                            renderScene={this._renderScene}
+                            onIndexChange={index => this.setState({ index })}
+                            initialLayout={{ width: Dimensions.get('window').width }}
+                            style={{ height: Dimensions.get('screen').height / 2 - 90 }}
+                            renderTabBar={this._renderTabBar}
+
+                        />
+                    </>
+                }
             </MainLayout>
         );
     }
