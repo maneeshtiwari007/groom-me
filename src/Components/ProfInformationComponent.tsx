9d1c8e8837c3606e29d53d3e7b71a1b2f60ed6ad
subhash018 <59439792+subhash018@users.noreply.github.com>
2024-01-31 15:38:25 +0530

    Prof page
    Prof inside details tab work

--- src/Components/ProfInformationComponent.tsx
+++ src/Components/ProfInformationComponent.tsx
@@ -1,7 +1,11 @@
 import { Component } from "react";
 import ScreenInterfcae from "../Interfaces/Common/ScreensInterface";
 import CommonScreenStateInterface from "../Interfaces/States/CommonScreenStateInterface";
-import { Text, View } from "react-native";
+import { Text, View, Pressable, Image, ScrollView } from "react-native";
+import { ThemeStyling } from "../utilty/styling/Styles";
+import { FontAwesome, Ionicons, MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+import Colors from "../utilty/Colors";
+import { CommonHelper } from "../utilty/CommonHelper";
 
 export default class ProfInformationComponent extends Component<ScreenInterfcae, CommonScreenStateInterface>{
     constructor(props: any) {
@@ -11,6 +15,13 @@ export default class ProfInformationComponent extends Component<ScreenInterfcae,
         }
     }
     render() {
-        return<View><Text>Information</Text></View>
-}
+        return <ScrollView style={{ paddingTop: 10 }}>
+            <View style={[ThemeStyling.card, { backgroundColor: Colors.gray100, marginBottom: 5 }]}>
+                <View style={[ThemeStyling.cardBody]}>
+                    <Text style={[ThemeStyling.heading5, { fontSize: Colors.FontSize.h6, fontWeight: '600', color: Colors.dark_color, marginBottom: 0 }]}>About</Text>
+                    <Text style={[ThemeStyling.text1, { fontSize: Colors.FontSize.f12 }]}>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet</Text>
+                </View>
+            </View>
+        </ScrollView>
+    }
 }
--- src/Components/ProfReviewComponent.tsx
+++ src/Components/ProfReviewComponent.tsx
@@ -1,7 +1,11 @@
 import { Component } from "react";
 import ScreenInterfcae from "../Interfaces/Common/ScreensInterface";
 import CommonScreenStateInterface from "../Interfaces/States/CommonScreenStateInterface";
-import { Text, View } from "react-native";
+import { Text, View, Pressable, Image, ScrollView } from "react-native";
+import { ThemeStyling } from "../utilty/styling/Styles";
+import { FontAwesome, Ionicons, MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
+import Colors from "../utilty/Colors";
+import { CommonHelper } from "../utilty/CommonHelper";
 
 export default class ProfReviewComponent extends Component<ScreenInterfcae, CommonScreenStateInterface>{
     constructor(props: any) {
@@ -11,6 +15,157 @@ export default class ProfReviewComponent extends Component<ScreenInterfcae, Comm
         }
     }
     render() {
-        return<View><Text>Reviews</Text></View>
-}
+        return <ScrollView style={[ThemeStyling.container]}>
+            <Pressable style={[ThemeStyling.card, { backgroundColor: Colors.gray100, borderColor:Colors.gray400, borderStyle:"solid", borderWidth:1, borderRadius:10 }]}>
+                <View style={ThemeStyling.cardHeader}>
+                    <View style={[ThemeStyling.twoColumnLayout]}>
+                        <View style={[{ marginRight: 5 }]}>
+                            <Image style={[ThemeStyling.cardImage, { width: 55, height:55, borderRadius: 100 }]} source={require('../../assets/staticimages/default.jpg')} />
+                        </View>
+                        <View style={[ThemeStyling.col10, { padding: 8, paddingVertical: 0 }]}>
+                            <View style={{}}>
+                                <Text style={[ThemeStyling.heading5, { fontSize: Colors.FontSize.h6, fontWeight: '600', color: Colors.dark_color, marginBottom: 0 }]}>Cameron Willamson</Text>
+                                <View style={[ThemeStyling.starRating, { marginBottom: 5 }]}>
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.gray400} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.gray400} />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View style={[ThemeStyling.cardBody, { paddingVertical: 8, backgroundColor: Colors.white, borderBottomLeftRadius:10, borderBottomRightRadius:10 }]}>
+                    <Text style={[ThemeStyling.text1, { fontSize: Colors.FontSize.f12 , marginBottom:0}]}>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    </Text>
+                </View>
+            </Pressable>
+            <Pressable style={[ThemeStyling.card, { backgroundColor: Colors.gray100, borderColor:Colors.gray400, borderStyle:"solid", borderWidth:1, borderRadius:10 }]}>
+                <View style={ThemeStyling.cardHeader}>
+                    <View style={[ThemeStyling.twoColumnLayout]}>
+                        <View style={[{ marginRight: 5 }]}>
+                            <Image style={[ThemeStyling.cardImage, { width: 55, height:55, borderRadius: 100 }]} source={require('../../assets/staticimages/default.jpg')} />
+                        </View>
+                        <View style={[ThemeStyling.col10, { padding: 8, paddingVertical: 0 }]}>
+                            <View style={{}}>
+                                <Text style={[ThemeStyling.heading5, { fontSize: Colors.FontSize.h6, fontWeight: '600', color: Colors.dark_color, marginBottom: 0 }]}>Cameron Willamson</Text>
+                                <View style={[ThemeStyling.starRating, { marginBottom: 5 }]}>
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.gray400} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.gray400} />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View style={[ThemeStyling.cardBody, { paddingVertical: 8, backgroundColor: Colors.white, borderBottomLeftRadius:10, borderBottomRightRadius:10 }]}>
+                    <Text style={[ThemeStyling.text1, { fontSize: Colors.FontSize.f12 , marginBottom:0}]}>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    </Text>
+                </View>
+            </Pressable>
+            <Pressable style={[ThemeStyling.card, { backgroundColor: Colors.gray100, borderColor:Colors.gray400, borderStyle:"solid", borderWidth:1, borderRadius:10 }]}>
+                <View style={ThemeStyling.cardHeader}>
+                    <View style={[ThemeStyling.twoColumnLayout]}>
+                        <View style={[{ marginRight: 5 }]}>
+                            <Image style={[ThemeStyling.cardImage, { width: 55, height:55, borderRadius: 100 }]} source={require('../../assets/staticimages/default.jpg')} />
+                        </View>
+                        <View style={[ThemeStyling.col10, { padding: 8, paddingVertical: 0 }]}>
+                            <View style={{}}>
+                                <Text style={[ThemeStyling.heading5, { fontSize: Colors.FontSize.h6, fontWeight: '600', color: Colors.dark_color, marginBottom: 0 }]}>Cameron Willamson</Text>
+                                <View style={[ThemeStyling.starRating, { marginBottom: 5 }]}>
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.gray400} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.gray400} />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View style={[ThemeStyling.cardBody, { paddingVertical: 8, backgroundColor: Colors.white, borderBottomLeftRadius:10, borderBottomRightRadius:10 }]}>
+                    <Text style={[ThemeStyling.text1, { fontSize: Colors.FontSize.f12 , marginBottom:0}]}>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    </Text>
+                </View>
+            </Pressable>
+            <Pressable style={[ThemeStyling.card, { backgroundColor: Colors.gray100, borderColor:Colors.gray400, borderStyle:"solid", borderWidth:1, borderRadius:10 }]}>
+                <View style={ThemeStyling.cardHeader}>
+                    <View style={[ThemeStyling.twoColumnLayout]}>
+                        <View style={[{ marginRight: 5 }]}>
+                            <Image style={[ThemeStyling.cardImage, { width: 55, height:55, borderRadius: 100 }]} source={require('../../assets/staticimages/default.jpg')} />
+                        </View>
+                        <View style={[ThemeStyling.col10, { padding: 8, paddingVertical: 0 }]}>
+                            <View style={{}}>
+                                <Text style={[ThemeStyling.heading5, { fontSize: Colors.FontSize.h6, fontWeight: '600', color: Colors.dark_color, marginBottom: 0 }]}>Cameron Willamson</Text>
+                                <View style={[ThemeStyling.starRating, { marginBottom: 5 }]}>
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.gray400} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.gray400} />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View style={[ThemeStyling.cardBody, { paddingVertical: 8, backgroundColor: Colors.white, borderBottomLeftRadius:10, borderBottomRightRadius:10 }]}>
+                    <Text style={[ThemeStyling.text1, { fontSize: Colors.FontSize.f12 , marginBottom:0}]}>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    </Text>
+                </View>
+            </Pressable>
+            <Pressable style={[ThemeStyling.card, { backgroundColor: Colors.gray100, borderColor:Colors.gray400, borderStyle:"solid", borderWidth:1, borderRadius:10 }]}>
+                <View style={ThemeStyling.cardHeader}>
+                    <View style={[ThemeStyling.twoColumnLayout]}>
+                        <View style={[{ marginRight: 5 }]}>
+                            <Image style={[ThemeStyling.cardImage, { width: 55, height:55, borderRadius: 100 }]} source={require('../../assets/staticimages/default.jpg')} />
+                        </View>
+                        <View style={[ThemeStyling.col10, { padding: 8, paddingVertical: 0 }]}>
+                            <View style={{}}>
+                                <Text style={[ThemeStyling.heading5, { fontSize: Colors.FontSize.h6, fontWeight: '600', color: Colors.dark_color, marginBottom: 0 }]}>Cameron Willamson</Text>
+                                <View style={[ThemeStyling.starRating, { marginBottom: 5 }]}>
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.gray400} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.gray400} />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View style={[ThemeStyling.cardBody, { paddingVertical: 8, backgroundColor: Colors.white, borderBottomLeftRadius:10, borderBottomRightRadius:10 }]}>
+                    <Text style={[ThemeStyling.text1, { fontSize: Colors.FontSize.f12 , marginBottom:0}]}>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    </Text>
+                </View>
+            </Pressable>
+            <Pressable style={[ThemeStyling.card, { backgroundColor: Colors.gray100, borderColor:Colors.gray400, borderStyle:"solid", borderWidth:1, borderRadius:10 }]}>
+                <View style={ThemeStyling.cardHeader}>
+                    <View style={[ThemeStyling.twoColumnLayout]}>
+                        <View style={[{ marginRight: 5 }]}>
+                            <Image style={[ThemeStyling.cardImage, { width: 55, height:55, borderRadius: 100 }]} source={require('../../assets/staticimages/default.jpg')} />
+                        </View>
+                        <View style={[ThemeStyling.col10, { padding: 8, paddingVertical: 0 }]}>
+                            <View style={{}}>
+                                <Text style={[ThemeStyling.heading5, { fontSize: Colors.FontSize.h6, fontWeight: '600', color: Colors.dark_color, marginBottom: 0 }]}>Cameron Willamson</Text>
+                                <View style={[ThemeStyling.starRating, { marginBottom: 5 }]}>
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.primary_color} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.gray400} />
+                                    <FontAwesome style={[ThemeStyling.iconStar, { fontSize: Colors.FontSize.f12 , marginBottom:0}]} name="star" color={Colors.gray400} />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View style={[ThemeStyling.cardBody, { paddingVertical: 8, backgroundColor: Colors.white, borderBottomLeftRadius:10, borderBottomRightRadius:10 }]}>
+                    <Text style={[ThemeStyling.text1, { fontSize: Colors.FontSize.f12 , marginBottom:0}]}>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    </Text>
+                </View>
+            </Pressable>
+        </ScrollView>
+    }
 }
